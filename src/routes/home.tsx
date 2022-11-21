71f0362ce5cadf0e1b160ff4e98676150a200d67
vanderrich <67942051+vanderrich@users.noreply.github.com>
2022-11-21 20:10:03 +0700

    reviews

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,32 @@
-import type { Component } from 'solid-js';
+import { Component, For, Index } from 'solid-js';
 import styles from "../App.module.css";
+import reviews from "../assets/reviews.json";
+import star from "../assets/star.svg";
+import halfStar from "../assets/half star.svg";
 
 const Home: Component = () => {
     return (
         <div class={styles.home}>
             <h1>Brown House</h1>
-        </div>
+            <For each={reviews}>
+                {(review) => (
+                    <div class={styles.review}>
+                        <div>
+                            "{review.quote}"<br />
+                            <Index each={Array.from(Array(Math.floor(review.rating)).keys())}>
+                                {() => (
+                                    <img src={star} />
+                                )}
+                            </Index>
+                            {review.rating % 1 == 0.5 ? (<><img src={halfStar} /><br /></>) : ""}
+                            ~ {review.name}<br />{review.class}<br />
+                        </div>
+                        <img src={`/src/assets/pics of happy customers/${review.name} ${review.classShort}.jpg`} width="100vw" height="100vh" />
+                    </div>
+                )
+                }
+            </For >
+        </div >
     )
 };
 
