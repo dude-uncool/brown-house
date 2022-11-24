896ea111642426bb47c816f9a86228895fbeae84
vanderrich <67942051+vanderrich@users.noreply.github.com>
2022-11-24 20:57:44 +0700

    images

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,11 +3,22 @@ import styles from "../App.module.css";
 import halfStar from "../assets/half star.svg";
 import reviews from "../assets/reviews.json";
 import star from "../assets/star.svg";
+import JCDImg from "../assets/1 JCD.png"
+import { style } from 'solid-js/web';
 
 const Home: Component = () => {
     return (
         <div class={styles.home}>
             <h1>Brown House</h1>
+            <div class={styles.images}>
+                <div>
+                    <img src={JCDImg} class={styles.leftImg} />
+                    <h2 class={styles.ImgDesc}>JCD</h2>
+                    <span class={styles.ImgDesc}>Jelly Chocolate Drink</span>
+                    <span class={styles.ImgDesc}>Chocolate Milk with Jelly</span>
+                </div>
+            </div>
+            <div class={styles.whitespace} />
             <div class={styles.reviews}>
                 <For each={reviews}>
                     {(review) => (
